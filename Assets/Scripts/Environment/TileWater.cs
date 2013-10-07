843180d2f5a761f3fd1bd7bf5b3eb23b97668019
Martin Fasterholdt <mfasterholdt@gmail.com>
2013-10-07 23:54:00 +0200

    Renamed WaterTile script, added gizmo to water tile, added prefab

---
+++ Assets/Scripts/Environment/TileWater.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TileWater : EnvironmentTile 
+{
+	Color gizmoColour = new Color(0.1f, 0.35f, 0.9f, 0.5f);
+	Vector3 gizmoSize = new Vector3(1, 0.4f, 1);
+	
+	void OnDrawGizmos()
+	{
+		if(!Application.isPlaying)
+		{
+			Gizmos.color = gizmoColour;
+			Vector3 pos = transform.position;
+			pos.y = -0.5f; 
+			Gizmos.DrawCube(pos, gizmoSize);
+		}
+	}
+}
