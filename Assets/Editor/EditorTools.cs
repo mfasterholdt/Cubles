8315aaaccddd353960fde58902ebf72440c7684a
Martin Fasterholdt <mfasterholdt@gmail.com>
2013-10-21 23:40:43 +0200

    EditorTools added, reset position, reset rotation+scale

---
+++ Assets/Editor/EditorTools.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+public class EditorTools  
+{
+	[MenuItem("GameObject/Reset/LocalPosition %#r")]
+	static void ResetTransform()
+	{
+		GameObject obj = UnityEditor.Selection.activeGameObject;
+		
+		if(obj != null)
+		{
+			Undo.RegisterUndo(obj,"Reset Local Position");
+			obj.transform.localPosition = Vector3.zero;
+		}
+	}
+	
+	[MenuItem("GameObject/Reset/OrientationAndScale %#e")]
+	static void ResetOrientationAndScale()
+	{
+		GameObject obj = UnityEditor.Selection.activeGameObject;
+		
+		if(obj != null)
+		{
+			Undo.RegisterUndo(obj,"Reset Local Rotation and Local Scale");
+			obj.transform.localRotation = Quaternion.identity;
+			obj.transform.localScale = Vector3.one;
+		}
+	}
+}
