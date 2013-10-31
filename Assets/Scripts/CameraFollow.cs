d6636b3674740e0f210806d88bd92850fbccb615
Martin Fasterholdt <mfasterholdt@gmail.com>
2013-10-31 23:00:42 +0100

    Camera Follow Added

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour 
+{
+	public GameObject target;
+	public float speed = 5f;
+	
+	private Vector3 offset;
+	
+	void Start()
+	{
+		if(target != null)
+		{
+			offset = target.transform.position - transform.position;
+		}
+	}
+	
+	void FixedUpdate () 
+	{
+		if(target != null)
+		{
+			Vector3 camPos = transform.position;
+			
+			camPos += (target.transform.position - camPos - offset) * Time.deltaTime * speed;
+			
+			transform.position = camPos;
+		}
+	}
+}
